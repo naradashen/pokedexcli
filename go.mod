4b13b61cd68ad537c7cb9df34eb7b6b18eb95ff2
Narada Kandawala <109290698+naradashen@users.noreply.github.com>
2024-07-11 21:09:47 +0000

    Add files via upload

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/naradashen/pokedexcli
+
+go 1.22.3
---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+    "flag"
+    "fmt"
+    "log"
+    "os"
+    "strconv"
+
+    "github.com/naradashen/pokedexcli/internal/cli" // Ensure correct import path
+)
+
+func main() {
+    cli.Run()
+}
